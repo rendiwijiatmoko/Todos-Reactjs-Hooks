9295a881bd879113cdfa626fbb89365640d6c690
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2019-12-13 14:45:06 +0700

    useForm

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import useForm from '../customeHooks/useForm';
 
 const TodoAdd = ({saveTodo}) => {
-    const [text, setText] = useState("");
+    // const [text, setText] = useState("");
+    const {value, onChange, reset} = useForm("");
 
     return (
         <form
             onSubmit={(event) => {
                 event.preventDefault();
-                saveTodo(text);
-                setText("")
+                saveTodo(value);
+                reset();
             }}
         >
             <TextField
                 variant="outlined"
                 placeholder="Add todo"
                 margin="normal"
-                onChange={(event) => {
-                    setText(event.target.value)
-                }}
-                value={text}
+                onChange={onChange}
+                value={value}
             />
         </form>
     );
